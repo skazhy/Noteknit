d4e34b077d4df3bfb0a24dc2130d78a658dbbeef
Karlis Lauva <skazhy@gmail.com>
2012-10-27 03:50:22 +0300

    output positions on change

--- static/js/noteknit.js
+++ static/js/noteknit.js
@@ -5,21 +5,38 @@ $(function() {
     var Song = Backbone.Collection.extend({
         urlRoot: '/song',
         url: '/song',
-        model: Note
+        model: Note,
+        outputCode: function() {
+            console.log(this.map(function(e) { return e.get("el").position(); }));
+        }
     });
     
-    var NoteView = Backbone.View.extend({
+    var song = new Song();
+    song.on("add", function() {
+        this.outputCode();
     });
+    song.on("destroy", function() {
+        this.outputCode();
+    });
+    
     $("#notebox").draggable({ handle: "#mover" });
     $('.note').draggable({
         revert: "invalid",
         helper: "clone",
     }).bind('dragstop', function(event, ui) {
-     // TODO: add to song here
-     $(this).after($(ui.helper)
-        .clone()
-        .draggable({ revert: "invalid", grid: [10, 20] })
-        .bind("dblclick", function() { $(this).remove(); }));
+     var note = new Note();
+     var copy = $(ui.helper).clone();
+     copy.attr("id", note.cid);
+     copy.bind("dragstop", function() {
+         song.outputCode();
+     });
+     note.set("el", copy);
+     song.add(note);
+     copy.draggable({ revert: "invalid", grid: [10, 20] }).bind("dblclick", function() { 
+         song.getByCid($(this).attr("id")).destroy();
+         $(this).remove(); });
+
+     $(this).after(copy);
     });
     
     $('.dest').droppable({ });
